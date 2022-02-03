7badf8df5869adac708ea060af58f492c7c41f37
Haroutioun <haroutioun24@gmail.com>
2022-02-03 21:03:17 +0100

    fixing unitests

--- src/hello_fastapi.py
+++ src/hello_fastapi.py
@@ -1,7 +1,7 @@
 """This file is just to play with fastAPI"""
 
 from fastapi import FastAPI
-from img_px_calculator import *
+from src.img_px_calculator import *
 from pydantic import BaseModel
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
@@ -33,7 +33,7 @@ def home():
 def getSize():
     url = 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg'
     sizeInPx = get_nb_px_in_img_from_url(url)
-    return {"Size": sizeInPx}
+    return {"size": sizeInPx}
 
 @app.post("/get-size-from-url/")
 def getSizeFromUrl(item: Item):
